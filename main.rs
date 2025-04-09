f2955933d5e9e8140d28380dd5b2eb09bfd21c9e
Kevin Huang <140923263+fellowfan@users.noreply.github.com>
2025-04-09 16:03:10 -0400

    Create main.rs
    Boilerplate code for now

---
+++ main.rs
@@ -0,0 +1,22 @@
+#![allow(non_snake_case,non_camel_case_types,dead_code)]
+
+/*
+    Fill in the polarity function below. Use as many helpers as you want.
+    Test your code by running 'cargo test' from the tester_rs_simple directory.
+    
+*/
+
+fn polarity(board: & [&str], specs: & (Vec<i32>, Vec<i32>, Vec<i32>, Vec<i32>)) -> Vec<String>
+{
+    // 5x6 solution hardcoded to demonstrate return type
+    vec![ String::from("+-+-X-"), 
+          String::from("-+-+X+"), 
+          String::from("XX+-+-"), 
+          String::from("XX-+X+"), 
+          String::from("-+XXX-") ]
+}
+    
+#[cfg(test)]
+#[path = "tests.rs"]
+mod tests;
+
